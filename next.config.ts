46aac9d09a191cceb800dc48479e17eb2991a43c
jeffyang400 <56237675+jeffyang400@users.noreply.github.com>
2025-03-20 14:12:35 -0700

    Fix next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/nextjs-website' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-website/out' : '',
   images: {
     unoptimized: true,
   },
