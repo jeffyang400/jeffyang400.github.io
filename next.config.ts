1df7555120453a0db516a752fc4754e8478bea17
jeffyang400 <56237675+jeffyang400@users.noreply.github.com>
2025-03-20 13:03:41 -0700

    Added deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/nextjs-github-pages",
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/nextjs-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/nextjs-website' : '',
   images: {
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
