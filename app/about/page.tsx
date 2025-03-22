db68826f77609d464be2ea8b16d80c9462cde4b8
jeffyang400 <56237675+jeffyang400@users.noreply.github.com>
2025-03-21 19:23:24 -0700

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,22 +2,52 @@
 
 import { useRouter } from 'next/navigation';
 import { JSX } from 'react';
+import Image from 'next/image';
 
 export default function About(): JSX.Element {
   const router = useRouter();
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-3xl font-bold mb-4">About Jeffrey</h1>
-      <p className="text-lg text-gray-700 mb-6 max-w-lg text-center">
-        This is the About page
+      <h1 className="text-5xl font-bold mb-4 text-center">This is Jeffrey</h1>
+      <Image 
+        src="/nextjs-website/profile.jpg" 
+        alt="Profile Picture" 
+        width={192}
+        height={192}
+        className="object-cover mb-6 rounded-lg shadow-lg"
+      />
+      <p className="text-lg text-white mb-6 max-w-lg text-center">
+        Jeffrey is a Software Engineer Intern @ BawsHuman
       </p>
-      <button 
-        className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
-        onClick={() => router.push('/')}
-      >
-        Go Back to Home
-      </button>
+
+      <div className="flex space-x-6">
+      <a href="https://www.linkedin.com/in/jeffrey-yang-9a9139186/" target="_blank" rel="noopener noreferrer">
+        <Image
+            src="/nextjs-website/linkedin-logo.png" 
+            alt="Go to Jeffrey's LinkedIn" 
+            width={100}
+            height={100}
+            className="object-cover rounded-lg shadow-lg hover:opacity-80 transition cursor-pointer"
+          />
+        </a>
+
+        <a href="https://github.com/jeffyang400" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/nextjs-website/git-logo.png" 
+            alt="Go to Jeffrey's Github" 
+            width={100}
+            height={100}
+            className="object-cover rounded-lg shadow-lg hover:opacity-80 transition cursor-pointer"
+          />
+        </a>
+      </div>
+      <div className="text-lg text-gray-700 mt-8 max-w-lg text-center">
+        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
+         onClick={() => router.push('/')}>
+          Go Back
+        </button>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import { JSX } from 'react';
+import Image from 'next/image';
 
 export default function Home(): JSX.Element {
   const router = useRouter();
@@ -9,19 +10,21 @@ export default function Home(): JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-3xl font-bold mb-4">Home</h1>
-      <img 
+      <Image
         src="/nextjs-website/profile.jpg" 
         alt="Profile Picture" 
-        className="w-48 h-48 object-cover mb-6 rounded-lg shadow-lg"
+        width={192}
+        height={192}
+        className="object-cover mb-6 rounded-lg shadow-lg"
       />
-      <p className="text-lg text-gray-700 mb-6 max-w-lg text-center">
+      <p className="text-lg text-white mb-6 max-w-lg text-center">
         This is a website about Jeffrey Yang
       </p>
       <button 
         className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
         onClick={() => router.push('/about')}
       >
-        Click here for more about Jeffrey
+        Learn more
       </button>
     </div>
   );
