f8ca6f78f2914cbeee5b73a6ee96d71d677eaf07
jeffyang400 <56237675+jeffyang400@users.noreply.github.com>
2025-03-23 14:58:18 -0700

    Update next.config to root

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/jeffyang400.github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/jeffyang400.github.io' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
   images: {
     unoptimized: true,
   },
